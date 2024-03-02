050b43472c93f6dcfba00b532fd8738d886ea602
Yuval Rozner <yuvalrozner98@gmail.com>
2024-03-02 18:18:29 +0200

    Update Head.jsx

--- src/Head.jsx
+++ src/Head.jsx
@@ -1,7 +1,7 @@
 const Head = () => {
   return (
     <img
-      src="https://github.com/YuvalRozner/Icook/blob/main/public/logo.webp"
+      src="/public/logo.webp"
       alt="Website Logo"
       className="w-72 max-w-full max-h-full mx-auto my-4 bg-white shadow-2xl relative rounded-3xl object-cover h-40 w-50 "
     ></img>
