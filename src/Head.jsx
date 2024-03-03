349e19ef00053301442a5aab597cf425760cf04a
Yuval Rozner <yuvalrozner98@gmail.com>
2024-03-03 11:09:07 +0200

    Update Head.jsx

--- src/Head.jsx
+++ src/Head.jsx
@@ -2,7 +2,7 @@ const Head = () => {
   return (
     <div className="flex justify-center items-center mx-auto my-4 max-w-full">
       <img
-        src="images/logo1.jpg"
+        src="logo1.jpg"
         alt="Website Logo"
         className="w-auto max-w-full h-auto max-h-48 bg-white relative object-cover"
       />
