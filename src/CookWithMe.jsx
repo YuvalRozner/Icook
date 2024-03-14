da0d0030db26b555eff723c02913241981a4e820
Yuval Rozner <yuvalrozner98@gmail.com>
2024-03-14 19:47:24 +0200

    Update CookWithMe.jsx

--- src/CookWithMe.jsx
+++ src/CookWithMe.jsx
@@ -36,43 +36,45 @@ function CookWithMe({ data }) {
     <>
       <NavigationBar title={title} />
       <div className="flex flex-col items-center md:mt-5">
-        <div className="flex justify-between items-center w-auto mb-2 text-2xl">
+        <div className="flex justify-between items-center w-auto mt-2 md:mb-2 text-xl md:text-2xl">
           <button
             onClick={handlePrev}
             disabled={currentStep === 0}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 md:py-2 md:px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
           >
             Prev
           </button>
-          <h2 className="text-3xl font-semibold mx-20 sm:mx-20 md:mx-20 lg:mx-40 xl:mx-50">
+          <h2 className="text-xl md:text-3xl font-semibold mx-20 sm:mx-20 md:mx-20 lg:mx-40 xl:mx-50">
             Step {currentStep + 1} of {instructions.length}
           </h2>
           <button
             onClick={handleNext}
             disabled={currentStep === instructions.length - 1}
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-2 md:py-2 md:px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed"
           >
             Next
           </button>
         </div>
         {/* Progress Bar */}
-        <div className="text-xl text-cyan-800 font-semibold">
+        <div className="md:text-2xl text-cyan-800 font-semibold mb-2">
           {"estimate cooking progress: " + Math.round(progressPercentage) + "%"}
         </div>
-        <div className="w-1/3  bg-gray-200 rounded-full h-4 dark:bg-gray-700 mb-4">
+        <div className="w-10/12 md:w-2/5 bg-gray-200 rounded-full h-4 md:h-5 dark:bg-gray-700 mb-4">
           <div
-            className="bg-green-600 h-4 rounded-full"
+            className="bg-green-600 h-4 md:h-5 rounded-full"
             style={{ width: `${progressPercentage}%` }}
           ></div>
         </div>
-        <p className="mb-4 px-4 text-center text-2xl">
+        <p className="mb-4 px-4 text-center text-xl md:text-3xl">
           {instructions[currentStep]}
         </p>
-        <img
-          src={`/Icook/${title}_${currentStep + 1}.jpg`}
-          alt={`Step ${currentStep + 1}`}
-          className="max-w-lg h-auto rounded-md shadow-lg mb-4"
-        />
+        <div className="flex justify-center w-full px-3">
+          <img
+            src={`/Icook/${title}_${currentStep + 1}.jpg`}
+            alt={`Step ${currentStep + 1}`}
+            className="w-full md:w-2/5 h-72 md:h-96 object-cover rounded-md "
+          />
+        </div>
       </div>
     </>
   );
